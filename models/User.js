854381775d73a501bc59329f710afe98e9423f0c
Youssif Hamdy <youssifhamdy381@gmail.com>
2026-03-29 17:44:38 +0200

    fix: remove catchAsync from authController 2

--- models/User.js
+++ models/User.js
@@ -21,10 +21,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function hashPassword(next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function hashPassword() {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = async function comparePassword(candidate) {
